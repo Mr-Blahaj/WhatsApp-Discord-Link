ee693d0b9f8f9cff5e45bcdcb245b7897dfc9c97
Mr-Blahaj <121840358+Mr-Blahaj@users.noreply.github.com>
2023-01-04 17:42:23 +0530

    Added Icon
    Added Group Icon to thread

--- events/message_create.js
+++ events/message_create.js
@@ -8,11 +8,13 @@ module.exports = {
         const webhookClient = new Discord.WebhookClient({id: config.WebhookID,token: config.WebhookToken});
         const contact = await message.getContact();
         let chat = await message.getChat();
+        let contact_ = await chat.getContact();
         let thread = DiscordClient.channels.cache.get(config.ForumChannel).threads.cache.find((x) => x.name === `${chat.name}`);
         if (chat.id._serialized != 'status@broadcast') {
             if (!thread) {
                 thread = await webhookClient.send({
                     content: `${chat.id._serialized}`,
+                    files: [`${(await contact_.getProfilePicUrl()) ?? DefaultAvatar}`],
                     username: `WhatsApp`,
                     avatarURL: `https://cdn-icons-png.flaticon.com/512/124/124034.png`,
                     threadName: chat.name,
