9ffd2558e4381092ec94100c2559e39467c1000a
!Mr.Blahaj <121840358+Mr-Blahaj@users.noreply.github.com>
2023-01-22 20:30:03 +0530

    Update qr.js

--- events/qr.js
+++ events/qr.js
@@ -3,9 +3,7 @@ module.exports = {
     type: "whatsapp",
     async execute(DiscordClient, qr) {
         const config = require('../config.json')
-        require('qrcode-terminal').generate(qr, {
-            small: true
-        }, function (string) {
+        require('qrcode-terminal').generate(qr, {small: true}, function (string) {
             DiscordClient.channels.cache.get(config.LogChannel).send({embeds: [{color: 39423, description: `**WhatsApp Authentication Failed! Please Scan this!**\`\`\`${string}\`\`\``}]});
         })
     }
