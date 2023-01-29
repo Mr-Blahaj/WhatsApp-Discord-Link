872bf419609eb6394005f592662190ddc145352f
!Mr.Blahaj <121840358+Mr-Blahaj@users.noreply.github.com>
2023-01-29 20:30:03 +0530

    Update message_revoke_everyone.js
    It will not log deleted status now

--- events/message_revoke_everyone.js
+++ events/message_revoke_everyone.js
@@ -2,23 +2,26 @@ module.exports = {
     name: "message_revoke_everyone",
     type: "whatsapp",
     async execute(DiscordClient, message, revoked_msg) {
-        const config = require('../config.json')
-        const DefaultAvatar = 'https://i.ibb.co/KjssTPM/image-2023-01-02-211044093.png';
-        const contact = await message.getContact();
-        DiscordClient.channels.cache.get(config.DeletedMessagesChannel).send({
-            embeds: [
-                {
-                    color: 39423,
-                    author: {
-                        name: `Message Sent by ${contact.name ?? contact.pushname}`,
-                        icon_url: `${(await contact.getProfilePicUrl()) ?? DefaultAvatar}`,
+        let chat = await message.getChat();
+        if (chat.id._serialized != 'status@broadcast') {
+            const config = require('../config.json')
+            const DefaultAvatar = 'https://i.ibb.co/KjssTPM/image-2023-01-02-211044093.png';
+            const contact = await message.getContact();
+            DiscordClient.channels.cache.get(config.DeletedMessagesChannel).send({
+                embeds: [
+                    {
+                        color: 39423,
+                        author: {
+                            name: `Message Sent by ${contact.name ?? contact.pushname}`,
+                            icon_url: `${(await contact.getProfilePicUrl()) ?? DefaultAvatar}`,
+                        },
+                        description: `${revoked_msg.body}`,
+                        footer: {
+                            text: `${(await message.getChat()).name} || ID: ${message.from}`,
+                        },
                     },
-                    description: `${revoked_msg.body}`,
-                    footer: {
-                        text: `${(await message.getChat()).name} || ID: ${message.from}`,
-                    },
-                },
-            ],
-        });
+                ],
+            });
+        }
     }
 }
